5a9a78f508ced0a0ea5ab47cc2613c379640ba65
Steve Greatrex <stephen.greatrex@googlemail.com>
2013-09-30 22:08:46 +0100

    Initial implementation using nano

---
+++ docs.js
@@ -0,0 +1,28 @@
+﻿/*global exports:true*/
+(function (exports) {
+
+	exports.design = {
+		views: {
+			view1: {
+				map: function (doc) { emit(doc.id, null); }
+			},
+
+			view2: {
+				map: function () { },
+				reduce: function () { }
+			}
+		}
+	};
+
+	exports.design2 = {
+		views: {
+			view1: {
+				map: function () { }
+			}
+		},
+		filters: {
+			filter1: function() {}
+		}
+	};
+
+}(exports));
